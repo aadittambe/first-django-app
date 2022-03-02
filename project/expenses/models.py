07e1496e8d8de7698796e84e04eda9682a44cef6
Aadit Tambe <aadit.tambe@gmail.com>
2022-03-02 00:22:05 -0500

    hw

--- project/expenses/models.py
+++ project/expenses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Summary(models.Model):
     bioguide_id = models.CharField(max_length=7)
     office = models.CharField(max_length=500)
@@ -9,6 +10,10 @@ class Summary(models.Model):
     amount = models.DecimalField(max_digits=20, decimal_places=2)
     year = models.IntegerField()
     quarter = models.IntegerField()
+
+    def __str__(self):
+        return self.program
+
 
 class Detail(models.Model):
     bioguide_id = models.CharField(max_length=7)
@@ -26,3 +31,6 @@ class Detail(models.Model):
     purpose = models.CharField(max_length=500)
     amount = models.DecimalField(max_digits=20, decimal_places=2)
     year = models.IntegerField()
+
+    def __str__(self):
+        return self.payee
