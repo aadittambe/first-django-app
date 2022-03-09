5f0cd39ddfbc4ee8cd23a707a7104f6ca5c03c5c
Aadit Tambe <aadit.tambe@gmail.com>
2022-03-09 11:40:10 -0500

    todays changes

--- project/expenses/urls.py
+++ project/expenses/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # example: /summary/1/
+    path('categories/', views.categories, name='categories'),
+    path('summary/<int:summary_id>/', views.summary, name='summary'),
+    path('categories/<slug>/', views.category_detail, name='category_detail'),
     path('summary/<int:summary_id>/', views.summary, name='summary'),
 ]
--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
-from expenses.models import Summary, Detail
-
+from expenses.models import Summary, Detail, Category
 
 def index(request):
-    total_summaries = Summary.objects.count()
-    total_detail = Detail.objects.count()
-    return render(request, 'expenses/index.html', context={'total_summaries': total_summaries, 'total_detail': total_detail})
-
+  total_summaries = Summary.objects.count()
+  total_detail = Detail.objects.count()
+  return render(request, 'expenses/index.html', context={'total_summaries': total_summaries, 'total_detail': total_detail})
 
 def summary(request, summary_id):
-    summary = Summary.objects.get(id=summary_id)
-    return render(request, 'expenses/summary.html', {'summary': summary})
+  summary = Summary.objects.get(id=summary_id)
+  return render(request, 'expenses/summary.html', {'summary': summary})
+
+def categories(request):
+  categories = Category.objects.all().order_by('name')
+  return render(request, 'expenses/categories.html', {'categories': categories})
+
+def category_detail(request, slug):
+  category = get_object_or_404(Category, slug=slug)
+  category_summary = Summary.objects.filter(category=category.name).order_by('-amount')
+  top_5_summaries = category_summary[0:5]
+  summary_count = category_summary.count()
+  category_detail = Detail.objects.filter(category=category.name).count()
+  return render(request, 'expenses/category_detail.html', {'category': category, 'category_summary': category_summary, 'category_detail': category_detail, 'top_5_summaries': top_5_summaries})
